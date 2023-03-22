d17dd1f358e052b20e1dab82a3acc4b5b084bb7e
Karanelus <karane88vka@gmail.com>
2023-03-22 11:27:57 +0100

    ap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
 import "./App.css";
 
+let namesys = [
+  { name: "Ruslan Karaniewski", gay: false },
+  { name: "Leonardo Davinci", gay: true },
+  { name: "Fox Bernad", gay: true },
+];
+
+const List = (props) => {
+  let nameOfNotGays = props.names
+    .filter((nameX) => {
+      return !nameX.gay;
+    })
+    .map((nameX) => {
+      return <div key={nameX.name}>{nameX.name}</div>;
+    });
+  let nameOfGays = props.names
+    .filter((nameX) => {
+      return nameX.gay;
+    })
+    .map((nameX) => {
+      return <div key={nameX.name}>{nameX.name}</div>;
+    });
+
+  return (
+    <section>
+      {nameOfNotGays}
+      <br />
+      {nameOfGays}
+    </section>
+  );
+};
+
+// const List = (props) => {
+//   return (
+//     <div>
+//       {props.namesys.map((nameX) => {
+//         return <section key={nameX.name}>{nameX.name}</section>;
+//       })}
+//     </div>
+//   );
+// };
+
 function App() {
   return (
-    <div className="loco">
-      <h1>Hello</h1>
-      <ul>
-        <li>My name is Ruslan</li>
-        <li>I love programming</li>
-        <li>Make me happy</li>
-        <li>Enjoy yoursself</li>
-      </ul>
-    </div>
+    <>
+      <List names={namesys} />
+    </>
   );
 }
 
---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>Menu</li>
+        <li>Shop</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
