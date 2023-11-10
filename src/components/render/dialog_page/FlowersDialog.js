ce593a36dca86a43d088444bd087daf606956b70
Rusłan Karaniewski <karane88vka@gmail.com>
2023-11-10 11:05:03 +0100

    Added noreferree

--- src/components/render/dialog_page/FlowersDialog.js
+++ src/components/render/dialog_page/FlowersDialog.js
@@ -31,15 +31,15 @@ export const FlowersDialog = () => {
           <h2>This is portfolio page</h2>
           <p>
             If you enjoed by this page, please, contact with me on{" "}
-            <a href="https://www.linkedin.com/in/karanelus/" target="_blank" rel="noopener">
+            <a href="https://www.linkedin.com/in/karanelus/" target="_blank" rel="noreferrer">
               LinkedIn
             </a>
             ,{" "}
-            <a href="https://www.facebook.com/profile.php?id=100008296452572" target="_blank" rel="noopener">
+            <a href="https://www.facebook.com/profile.php?id=100008296452572" target="_blank" rel="noreferrer">
               Facebook
             </a>{" "}
             or{" "}
-            <a href="https://t.me/Karane1us" target="_blank" rel="noopener">
+            <a href="https://t.me/Karane1us" target="_blank" rel="noreferrer">
               Telegram
             </a>
             . I'm will be happy to connect and, hopefully, find my first Frontend Developer job
